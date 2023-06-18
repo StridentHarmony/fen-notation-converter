72b01f1bb759febd679a17df0a604adf64aafbe4
Asa <asaforpresident@gmail.com>
2023-06-17 19:22:16 -0700

    trimmed, replaced, and split

--- Converter.java
+++ Converter.java
@@ -1,8 +1,17 @@
 public class Converter {
-  static char[][] fenConverter(String notation) {
-    String[] fenArray = notation.split("/", 8);
+  static char[][] fenConverter(String fenNotation) {
+    String[] trimmedNotation = fenNotation.split(" ", 6);
 
-    
+    String stageOne=trimmedNotation[0].replace("1"," ");
+    String stageTwo=stageOne.replaceAll("2","  ");
+    String stageThree=stageTwo.replaceAll("3","   ");
+    String stageFour=stageThree.replaceAll("4","    ");
+    String stageFive=stageFour.replaceAll("5","     ");
+    String stageSix=stageFive.replaceAll("6","      ");
+    String stageSeven=stageSix.replaceAll("7","        ");
+    String stageEight=stageSeven.replaceAll("8","         ");
+
+    String[] fenArray = stageEight.split("/", 8);
     return ;
   }
 }
