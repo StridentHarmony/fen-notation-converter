503fedbf19bfd9c93879f0bdc82a375a109715d0
Asa <asaforpresident@gmail.com>
2023-06-17 19:27:34 -0700

    test case

--- Converter.java
+++ Converter.java
@@ -3,15 +3,21 @@ public class Converter {
     String[] trimmedNotation = fenNotation.split(" ", 6);
 
     String stageOne=trimmedNotation[0].replace("1"," ");
-    String stageTwo=stageOne.replaceAll("2","  ");
-    String stageThree=stageTwo.replaceAll("3","   ");
-    String stageFour=stageThree.replaceAll("4","    ");
-    String stageFive=stageFour.replaceAll("5","     ");
-    String stageSix=stageFive.replaceAll("6","      ");
-    String stageSeven=stageSix.replaceAll("7","        ");
-    String stageEight=stageSeven.replaceAll("8","         ");
+    String stageTwo=stageOne.replace("2","  ");
+    String stageThree=stageTwo.replace("3","   ");
+    String stageFour=stageThree.replace("4","    ");
+    String stageFive=stageFour.replace("5","     ");
+    String stageSix=stageFive.replace("6","      ");
+    String stageSeven=stageSix.replace("7","        ");
+    String stageEight=stageSeven.replace("8","         ");
 
     String[] fenArray = stageEight.split("/", 8);
+
+    
     return ;
+  }
+
+  public static void main(String[] args) {
+    System.out.println(fenConverter("k7/p2q4/3br3/8/7N/8/5K2/8 w - - 0 1"));
   }
 }
