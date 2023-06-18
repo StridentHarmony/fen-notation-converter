92bb196d204b470f1e5d1a9ccc78288d2d88dfb8
Asa <asaforpresident@gmail.com>
2023-06-17 22:44:18 -0700

    added spaces

--- Converter.java
+++ Converter.java
@@ -2,14 +2,14 @@ public class Converter {
   static char[][] fenConverter(String fenNotation) {
     String[] trimmedNotation = fenNotation.split(" ", 6);
 
-    String stageOne=trimmedNotation[0].replace("1"," ");
-    String stageTwo=stageOne.replace("2","  ");
-    String stageThree=stageTwo.replace("3","   ");
-    String stageFour=stageThree.replace("4","    ");
-    String stageFive=stageFour.replace("5","     ");
-    String stageSix=stageFive.replace("6","      ");
-    String stageSeven=stageSix.replace("7","        ");
-    String stageEight=stageSeven.replace("8","         ");
+    String stageOne = trimmedNotation[0].replace("1"," ");
+    String stageTwo = stageOne.replace("2","  ");
+    String stageThree = stageTwo.replace("3","   ");
+    String stageFour = stageThree.replace("4","    ");
+    String stageFive = stageFour.replace("5","     ");
+    String stageSix = stageFive.replace("6","      ");
+    String stageSeven = stageSix.replace("7","        ");
+    String stageEight = stageSeven.replace("8","         ");
 
     String[] fenArray = stageEight.split("/", 8);
 
