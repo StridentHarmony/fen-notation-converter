6c38e61669ad7f2e23fb221670e3e63039dd43ea
Asa <asaforpresident@gmail.com>
2023-06-17 19:32:28 -0700

    catch exceptions? maybe?

--- Converter.java
+++ Converter.java
@@ -1,19 +1,24 @@
 public class Converter {
   static char[][] fenConverter(String fenNotation) {
-    String[] trimmedNotation = fenNotation.split(" ", 6);
+    try {
+      String[] trimmedNotation = fenNotation.split(" ", 6);
 
-    String stageOne=trimmedNotation[0].replace("1"," ");
-    String stageTwo=stageOne.replace("2","  ");
-    String stageThree=stageTwo.replace("3","   ");
-    String stageFour=stageThree.replace("4","    ");
-    String stageFive=stageFour.replace("5","     ");
-    String stageSix=stageFive.replace("6","      ");
-    String stageSeven=stageSix.replace("7","        ");
-    String stageEight=stageSeven.replace("8","         ");
+      String stageOne=trimmedNotation[0].replace("1"," ");
+      String stageTwo=stageOne.replace("2","  ");
+      String stageThree=stageTwo.replace("3","   ");
+      String stageFour=stageThree.replace("4","    ");
+      String stageFive=stageFour.replace("5","     ");
+      String stageSix=stageFive.replace("6","      ");
+      String stageSeven=stageSix.replace("7","        ");
+      String stageEight=stageSeven.replace("8","         ");
 
-    String[] fenArray = stageEight.split("/", 8);
+      String[] fenArray = stageEight.split("/", 8);
 
-    
+      
+    } catch (PatternSyntaxException) {
+      System.out.println("Invalid FEN notation!");
+    }
+
     return ;
   }
 
