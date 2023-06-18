3d39fce173a97bc05bc84ac073f3f554205c5ac7
Asa <asaforpresident@gmail.com>
2023-06-17 20:18:46 -0700

    something's broken

--- Converter.java
+++ Converter.java
@@ -1,30 +1,27 @@
-import java.util.regex.PatternSyntaxException;
-
 public class Converter {
   static char[][] fenConverter(String fenNotation) {
-    try {
-      String[] trimmedNotation = fenNotation.split(" ", 6);
+    String[] trimmedNotation = fenNotation.split(" ", 6);
 
-      String stageOne=trimmedNotation[0].replace("1"," ");
-      String stageTwo=stageOne.replace("2","  ");
-      String stageThree=stageTwo.replace("3","   ");
-      String stageFour=stageThree.replace("4","    ");
-      String stageFive=stageFour.replace("5","     ");
-      String stageSix=stageFive.replace("6","      ");
-      String stageSeven=stageSix.replace("7","        ");
-      String stageEight=stageSeven.replace("8","         ");
+    String stageOne=trimmedNotation[0].replace("1"," ");
+    String stageTwo=stageOne.replace("2","  ");
+    String stageThree=stageTwo.replace("3","   ");
+    String stageFour=stageThree.replace("4","    ");
+    String stageFive=stageFour.replace("5","     ");
+    String stageSix=stageFive.replace("6","      ");
+    String stageSeven=stageSix.replace("7","        ");
+    String stageEight=stageSeven.replace("8","         ");
 
-      String[] fenArray = stageEight.split("/", 8);
+    String[] fenArray = stageEight.split("/", 8);
 
-
-    } catch (PatternSyntaxException e) {
-      System.out.println("Invalid FEN notation!");
+    char[][] board = new char[fenArray.length][];
+    for (int i = 0; i < fenArray.length; i++) {
+      board[i] = fenArray[i].toCharArray();
     }
 
-    return ;
+    return board;
   }
 
   public static void main(String[] args) {
-    System.out.println(fenConverter("k7/p2q4/3br3/8/7N/8/5K2/8 w - - 0 1"));
+    System.out.print(fenConverter("k7/p2q4/3br3/8/7N/8/5K2/8 w - - 0 1"));
   }
 }
